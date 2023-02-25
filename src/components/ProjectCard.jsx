4127706ea00f81236552eef4c877d213d284114c
shgawa13 <37065523+shgawa13@users.noreply.github.com>
2023-02-25 12:51:08 +0300

    font size
    Changing the font size

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,8 +5,8 @@ function ProjectCard({ item }) {
     <>
       <div className="img-box flex justify-center relative overflow-hidden text-center">
         <img src={item.img} alt="" />
-        <div className="p absolute top-[-100%] left-0 h-full w-full bg-[#00bbf9] p-4  lg:pt-24 tl:pt-24 sm:pt-20 xs:pt-14 ">
-          <h1 className="font-bold text-3xl">{item.title}</h1>
+        <div className="p absolute top-[-100%] left-0 h-full w-full bg-[#00bbf9] p-4 xl:pt-0 lg:pt-24 tl:pt-24 sm:pt-24 xs:pt-20 ">
+          <h1 className="font-bold text-3xl  xl:text-2xl tl:text-2xl">{item.title}</h1>
           <p className="font-bold">{item.description}</p>
         </div>
       </div>
